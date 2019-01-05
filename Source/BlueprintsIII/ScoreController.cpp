2e9c4a978fc17c307e6ecffb83bb50409885b287
Enrique Garrido Pozo <enrique.garrido@alu.uclm.es>
2019-01-05 16:48:26 +0100

    1st Delivery

---
+++ Source/BlueprintsIII/ScoreController.cpp
@@ -0,0 +1,56 @@
+#include "ScoreController.h"
+#include "Blueprint/UserWidget.h"
+#include "TextWidgetTypes.h"
+#include "TextBlock.h"
+
+// Sets default values
+AScoreController::AScoreController() :
+	score(0)
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AScoreController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (ScoreTextWidget) // Check if the Asset is assigned in the blueprint.
+	{
+		// Create the widget and store it.
+		pScoreTextWidget = CreateWidget<UUserWidget>(GetGameInstance(), ScoreTextWidget);
+		
+		// now you can use the widget directly since you have a referance for it.
+		// Extra check to  make sure the pointer holds the widget.
+		if (pScoreTextWidget.IsValid())
+		{
+			// let add it to the view port
+			pScoreTextWidget->AddToViewport();
+
+			// and retrieve the TextBlock widget within it
+			pScoreText = (UTextBlock*) pScoreTextWidget->GetWidgetFromName("ScoreText");
+		}
+	}
+}
+
+// Called every frame
+void AScoreController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AScoreController::IncreaseScore(int amount)
+{
+	if (pScoreText.IsValid()) {
+		score += amount;
+		pScoreText->SetText(FText::FromString(FString::FromInt(score)));
+	}
+}
+
+int AScoreController::GetScore() const
+{
+	return score;
+}
